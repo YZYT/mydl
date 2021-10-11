303e4f3b675bf43d4b56e8360ace0a96c97abb3d
YZYT <1565126883@qq.com>
2021-10-11 22:18:33 +0800

    test

--- utils/PSGD.py
+++ utils/PSGD.py
@@ -1,8 +1,34 @@
 from collections import defaultdict
-
+import torch.nn as nn
 import torch
 from torch.optim.optimizer import Optimizer
 import random
+
+class BasePSSGD(Optimizer):    
+    def __init__(self):
+        raise NotImplementedError
+    
+    def zero_grad(self):
+        self.optimizer.zero_grad()
+
+    def get_la_step(self):
+        return self._la_step
+
+    def state_dict(self):
+        return self.optimizer.state_dict()
+
+    def load_state_dict(self, state_dict):
+        self.optimizer.load_state_dict(state_dict)
+
+
+    def loss(self, y_hat, y):
+        return self.loss_fn(y_hat, y)
+
+
+    @property
+    def param_groups(self):
+        return self.optimizer.param_groups
+
 
 class Lookahead(Optimizer):
     r"""PyTorch implementation of the lookahead wrapper.
@@ -105,6 +131,10 @@ class Lookahead(Optimizer):
         return loss
 
 
+
+
+
+
 class PSSGDv1(Optimizer):
     r"""Version 1.0
     We update the fast parameter la_steps times, and then use the average as the update of slow parameter.
@@ -129,7 +159,6 @@ class PSSGDv1(Optimizer):
                 param_state = self.state[p]
                 for i in range(la_steps):
                     param_state['avg'] = torch.zeros_like(p.data)
-                    param_state['last_' + str(i)] = torch.zeros_like(p.data)
                     param_state['last_' + str(i)] = torch.zeros_like(p.data)
                     param_state['last_' + str(i)].copy_(p.data)
 
@@ -231,7 +260,6 @@ class PSSGDv2(Optimizer):
                 for i in range(la_steps):
                     param_state['avg'] = torch.zeros_like(p.data)
                     param_state['last_' + str(i)] = torch.zeros_like(p.data)
-                    param_state['last_' + str(i)] = torch.zeros_like(p.data)
                     param_state['last_' + str(i)].copy_(p.data)
 
 
@@ -327,7 +355,6 @@ class PSSGDv3(Optimizer):
                 for i in range(la_steps):
                     param_state['avg'] = torch.zeros_like(p.data)
                     param_state['last_' + str(i)] = torch.zeros_like(p.data)
-                    param_state['last_' + str(i)] = torch.zeros_like(p.data)
                     param_state['last_' + str(i)].copy_(p.data)
 
 
@@ -399,76 +426,125 @@ class PSSGDv3(Optimizer):
         return loss
 
 
-class PSSGDv4(Optimizer):
-    r"""Version 3.0
-    Randomly choose a past point
+class PSSGDv4(BasePSSGD):
+    r"""Version 4.0
+    multiple points
     """
 
-    def __init__(self, optimizer, la_steps=5, la_alpha=0.5, pullback_momentum="none"):
+    def __init__(self, optimizer, loss_fn=None, k=5, la_steps=5, la_alpha=0.5, pullback_momentum="none"):
         """optimizer: inner optimizer
         la_steps (int): number of lookahead steps
         la_alpha (float): linear interpolation factor. 1.0 recovers the inner optimizer.
         pullback_momentum (str): change to inner optimizer momentum on interpolation update
         """
         self.optimizer = optimizer
+        if loss_fn == None:
+            loss_fn = nn.MSELoss(reduction='mean')
+        self.loss_fn = loss_fn
         self._la_step = 0  # counter for inner optimizer
         self.la_alpha = la_alpha
         self._total_la_steps = la_steps
 
-        self.state = defaultdict(dict)
+        # number
+        self.k = k
+
+        # PSO state
+        self.age = 0
+        self.steps = 0
+        self.id = k
+        
+        self.state = [defaultdict(dict) for _ in range(k + 1)]
+        self.fitness = [-1 for _ in range(k + 1)]
 
         # Cache the current optimizer parameters
         for group in optimizer.param_groups:
-            for p in group['params']:
-                param_state = self.state[p]
-                for i in range(la_steps):
-                    param_state['avg'] = torch.zeros_like(p.data)
-                    param_state['last_' + str(i)] = torch.zeros_like(p.data)
-                    param_state['last_' + str(i)] = torch.zeros_like(p.data)
-                    param_state['last_' + str(i)].copy_(p.data)
+            for i in range(k + 1):
+                for p in group['params']:
+                    param_state = self.state[i][p]
+                    # param_state['avg'] = torch.zeros_like(p.data)
+                    # param_state['last_' + str(i)] = torch.zeros_like(p.data)
+                    # param_state['last_' + str(i)].copy_(p.data)
+                    param_state['weight'] = torch.zeros_like(p.data)
+                    param_state['weight'].copy_(p.data)
+                    param_state['age'] = 0   
+                    param_state['steps'] = 0        
 
 
     def __getstate__(self):
         return {
             'state': self.state,
             'optimizer': self.optimizer,
+            'k': self.k,
             'la_alpha': self.la_alpha,
             '_la_step': self._la_step,
             '_total_la_steps': self._total_la_steps,
         }
 
-    def zero_grad(self):
-        self.optimizer.zero_grad()
+    # def zero_grad(self):
+    #     self.optimizer.zero_grad()
 
-    def get_la_step(self):
-        return self._la_step
+    # def get_la_step(self):
+    #     return self._la_step
 
-    def state_dict(self):
-        return self.optimizer.state_dict()
+    # def state_dict(self):
+    #     return self.optimizer.state_dict()
 
-    def load_state_dict(self, state_dict):
-        self.optimizer.load_state_dict(state_dict)
+    # def load_state_dict(self, state_dict):
+    #     self.optimizer.load_state_dict(state_dict)
 
-    def _backup_and_load_cache(self):
-        """Useful for performing evaluation on the slow weights (which typically generalize better)
+
+    # @property
+    # def param_groups(self):
+    #     return self.optimizer.param_groups
+
+    def select_victim(self):
+        victim = 0
+        for i in range(self.k + 1):
+            if self.fitness[i] < 0:
+                return i
+            if self.fitness[i] > self.fitness[victim]:
+                victim = i
+        print(self.fitness)
+        return victim
+
+    def setloss(self, l):
+        victim = self.select_victim()
+        self.store_state(victim)
+        self.fitness[victim] = l
+
+    def select_father(self):
+        return 0
+
+    def _select_candicate(self):
+        return self.k
+    
+    def select_candicate(self):
+        # select an individual to grow
+        candicate = self._select_candicate()
+        self.load_state(candicate)
+
+
+    def store_state(self, k):
+        """
+        store the current position into kth buffer.
         """
         for group in self.optimizer.param_groups:
             for p in group['params']:
-                param_state = self.state[p]
-                param_state['backup_params'] = torch.zeros_like(p.data)
-                param_state['backup_params'].copy_(p.data)
-                p.data.copy_(param_state['cached_params'])
-
-    def _clear_and_load_backup(self):
+                param_state = self.state[k][p]
+                param_state['weight'].copy_(p.data)
+                param_state['age'] = self.age
+                param_state['steps'] = self.steps
+    
+    def load_state(self, k):
+        self.id = k
         for group in self.optimizer.param_groups:
             for p in group['params']:
-                param_state = self.state[p]
-                p.data.copy_(param_state['backup_params'])
-                del param_state['backup_params']
+                param_state = self.state[k][p]
+                p.data.copy_(param_state['weight'])
+                self.steps = param_state['steps']
+                self.age = param_state['age']
 
-    @property
-    def param_groups(self):
-        return self.optimizer.param_groups
+
 
     def step(self, closure=None):
         """Performs a single Lookahead optimization step.
@@ -476,26 +552,55 @@ class PSSGDv4(Optimizer):
             closure (callable, optional): A closure that reevaluates the model
                 and returns the loss.
         """
-        for group in self.optimizer.param_groups:
-            for p in group['params']:
-                param_state = self.state[p]
-                param_state['last_' + str(self._la_step)].copy_(p.data)
 
 
+        # ======= backup the current state =======
+
+
+
+        # ======= SGD step =======
+        # SGD step (individual grow)
         loss = self.optimizer.step(closure)
+        # print(f"loss: {loss}")
         self._la_step += 1
-        
-        if self._la_step >= self._total_la_steps:
+
+        self.age += 1
+        self.steps += 1
+
+        # for group in self.optimizer.param_groups:
+        #     for p in group['params']:
+        #         print(p.data)
+
+        # ======= evolution stage =======
+        if self._la_step >= self._total_la_steps * 10000:
             self._la_step = 0
-            # randomly select a point 
-            k = random.randint(0, self._total_la_steps - 1)
+
+            # find a victim
+            victim = self.select_victim()
+
+            # select an individual
+            father = self.select_father()
+
             for group in self.optimizer.param_groups:
                 for p in group['params']:
-                    param_state = self.state[p]
-                    p.data.mul_(self.la_alpha).add_(param_state['last_'+str(k)], alpha=1.0 - self.la_alpha)  # crucial line
+                    param_state_new = self.state[victim][p]
+                    param_state_fa = self.state[father][p]
+
+                    param_state_new['weight'].copy_(p.data.mul(self.la_alpha).add(param_state_fa['weight'], alpha=1.0-self.la_alpha))
+
+                    param_state_new['age'] = (self.age + param_state_fa['age']) / 2
+                    param_state_new['steps'] = 0 
+                    
+                    print(f"victim: {victim}")
+                    print(f"father: {father}")
+                    print(f"p.data: {p.data}")
+                    print(f"father: {param_state_fa['weight']}")
+                    print(f"new: {param_state_new['weight']}")
+
+                    # print("using")
+                    # print(self.state)
+        
         return loss
-
-
 
 # optimizer = # {any optimizer} e.g. torch.optim.Adam
 # if args.lookahead:
